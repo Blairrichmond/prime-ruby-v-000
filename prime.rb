d2829ff7931d9698998334dc247dcd7dd04e67a9
Brent  Ransom <sadumedusa@gmail.com>
2018-01-17 22:04:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
    range = (1...num + 1)
    div = []
    return false if num < 2
-   range.each do |i|
+   range.each do |x|
      div << x if num % x == 0
    end
    return div.all? { |i| i == 1 || i == num }
